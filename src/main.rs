844a0d03f5085d7cf82904b4648d16c8f1f7a3f6
dennyX7 <dennyxcodes@gmail.com>
2025-09-27 01:48:39 +0530

    feat(order): Define Side enum for market orders

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fmt::{self, Display, Formatter};
+
+// For market order
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Side {
+    Buy, // Bid
+    Sell, // Ask
+}
+
+impl Display for Side {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
 fn main() {
     print!("Hello, world!");
 }
