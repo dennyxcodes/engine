08b652e631691946db77829169af1cd95cd0c929
dennyX7 <dennyxcodes@gmail.com>
2025-09-27 01:54:47 +0530

    feat: Order structure with comprehensive Display formating

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt::{self, Display, Formatter};
+use std::fmt::{self, write, Display, Formatter};
 
 // For market order
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -11,6 +11,32 @@ impl Display for Side {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "{:?}", self)
     }
+}
+
+// Single Order placing
+#[derive(Debug, Clone)]
+pub struct Order {
+    pub order_id: u64,
+    pub symbol: String,
+    pub side: Side,
+    pub quantity: u64,
+    pub timestamp: u64,
+}
+
+impl Display for Order {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "ID: {}, {} {} @ {} (Qty: {}) | TS: {}",
+            self.order_id,
+            self.side,
+            self.symbol,
+            self.price,
+            self.quantity,
+            self.timestamp
+        )
+    }
+    
 }
 fn main() {
     print!("Hello, world!");
